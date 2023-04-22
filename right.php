4b94a2a39cef5f73ddb025761552918246a21b27
Dedan Irungu <dedanirungu@gmail.com>
2023-04-22 15:17:13 +0300

    Update rights

---
+++ right.php
@@ -0,0 +1,22 @@
+<?php
+
+$this->add_right("mailmass", "autoresponder", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("mailmass", "autoresponder", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("mailmass", "autoresponder", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("mailmass", "autoresponder", "staff", view:true, add:true, edit:true);
+$this->add_right("mailmass", "autoresponder", "registered", view:true, add:true);
+$this->add_right("mailmass", "autoresponder", "guest", view:true, );
+
+$this->add_right("mailmass", "campaign", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("mailmass", "campaign", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("mailmass", "campaign", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("mailmass", "campaign", "staff", view:true, add:true, edit:true);
+$this->add_right("mailmass", "campaign", "registered", view:true, add:true);
+$this->add_right("mailmass", "campaign", "guest", view:true, );
+
+$this->add_right("mailmass", "group", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("mailmass", "group", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("mailmass", "group", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("mailmass", "group", "staff", view:true, add:true, edit:true);
+$this->add_right("mailmass", "group", "registered", view:true, add:true);
+$this->add_right("mailmass", "group", "guest", view:true, );
