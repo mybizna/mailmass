c4423aac0fef4e124d3a719a2479646651583bc6
Dedan <dedanirungu@gmail.com>
2024-08-10 23:06:44 +0300

    Changes to files: Entities/Autoresponder.php Entities/Campaign.php Entities/Group.php

--- Entities/Campaign.php
+++ Entities/Campaign.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Modules\Mailmass\Entities;
+namespace Modules\Mailmass\Models;
 
-use Modules\Base\Entities\BaseModel;
+use Modules\Base\Models\BaseModel;
 
 class Campaign extends BaseModel
 {
