791f9e3a5624222ce8897fe2f2d47c52b48f7899
Dedan <dedanirungu@gmail.com>
2024-10-11 13:16:35 +0300

    Changes to files: app/Filament/Resources/AutoresponderResource.php app/Filament/Resources/CampaignResource.php app/Filament/Resources/GroupResource.php

--- app/Filament/Resources/GroupResource.php
+++ app/Filament/Resources/GroupResource.php
@@ -7,6 +7,7 @@ use Filament\Forms\Form;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Modules\Base\Filament\Resources\BaseResource;
+use Modules\Base\Filament\Resources\Pages;
 use Modules\Mailmass\Models\Group;
 
 class GroupResource extends BaseResource
@@ -77,4 +78,15 @@ class GroupResource extends BaseResource
             ]);
     }
 
+    public static function getPages(): array
+    {
+
+        Pages\ListBase::setResource(static::class);
+
+        return [
+            'index' => Pages\ListBase::route('/'),
+            'create' => Pages\CreateBase::route('/create'),
+            'edit' => Pages\EditBase::route('/{record}/edit'),
+        ];
+    }
 }
